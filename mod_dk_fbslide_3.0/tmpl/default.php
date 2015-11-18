c381acef5f41c605809cea4e4f1cc5b16e416eb7
Digikomp <chris.chaffey@digi-komp.com>
2015-11-18 16:39:04 +1030

    All files required for Joomla CMS 3.0
    All these files will only work on version 3.0 of Joomla.

---
+++ mod_dk_fbslide_3.0/tmpl/default.php
@@ -0,0 +1,39 @@
+<?php 
+/**
+ * Digi-Komp Facebook Slider. Module Entry Point
+ *
+ * Copyright (C) 2014 Chris Chaffey
+ *
+ * @package    Digi-Komp Facebook Slider
+ * @subpackage Modules
+ * @link       http://www.digi-komp.com
+ * @license    GNU/GPL, see license.txt
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
+
+$doc=JFactory::getDocument();
+if (trim( $params->get( 'loadCss' ) ) == 1)
+{
+	$doc->addStyleSheet(JURI::root().'modules/mod_dk_fbslide_3.0/css/mod_dk_fbslide_3.0.css');
+}
+else
+{
+	$doc->addStyleSheet(JURI::root().'modules/mod_dk_fbslide_3.0/css/mod_dk_fbslide_noncss_file.css');
+}
+
+if (trim( $params->get( 'loadjQuery' ) ) == 1)
+{
+	$doc->addScript(JURI::root().'media/jui/js/jquery.min.js');
+}
+?>
